572e6250bdb13be43b10c0eab20ffb7de874efaa
Ivo Zandhuis <ivo@zandhuis.nl>
2017-12-29 08:39:49 +0100

    Ecartico downloaded version 29 dec 2017

--- ecartico/getDataEcartico.py
+++ ecartico/getDataEcartico.py
@@ -17,8 +17,7 @@ anchor = infoboxcontent.find('a')
 href = anchor.get('href')
 numberOfPersons = int(href.replace('/ecartico/persons/', ""))
 
-numberOfPersons = 10 # overwrite to test
-numberOfPlaces = 0
+# numberOfPersons = 49 # overwrite to test
 
 # iterate and read the ecartico-person-uris
 for i in range(1, numberOfPersons + 1):
@@ -29,6 +28,29 @@ for i in range(1, numberOfPersons + 1):
     # read graph from uri
     result = g.parse(uri) # this function *adds* the new uri to graph g
 
+    # write every x persons into a file
+    x = 1000
+    if (i % x) == 0:
+        # serialize and write to file
+        s = g.serialize(format='turtle')
+        page = int(i/x)
+        filename = "ecartico" + str(page) + ".ttl"
+        file = open(filename,"wb")
+        file.write(s)
+        file.close()
+        g = rdflib.Graph()
+
+# serialize and write to file last
+if len(g) > 0:
+    s = g.serialize(format='turtle')
+    filename = "ecartico_rest.ttl"
+    file = open(filename,"wb")
+    file.write(s)
+    file.close()
+
+
+numberOfPlaces = 0
+g = rdflib.Graph()
 # iterate and read the ecartico-place-uris
 for i in range(1, numberOfPlaces):
     # construct uri
@@ -37,9 +59,3 @@ for i in range(1, numberOfPlaces):
 
     # read graph from uri
     result = g.parse(uri) # this function *adds* the new uri to graph g
-
-# serialize and write to file
-s = g.serialize(format='turtle')
-file = open("ecartico.ttl","wb")
-file.write(s)
-file.close()
